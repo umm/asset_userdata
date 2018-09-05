77dd4ac7356b8bf3e5e0a7ce398a90e6a4ce9bd2
Tetsuya Mori <monry84@gmail.com>
2018-09-05 17:48:43 +0900

    :baby: Initial commit

---
+++ Assets/Editor/Scripts/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace UnityModule.Asset
+{
+    /// <summary>
+    /// AssetImporter.userData に書き込むデータ構造
+    /// </summary>
+    /// <remarks>本来は各 umm で閉じさせるべき構造体だが、モジュール同士の AssetImporter.userData 上書き合戦を回避するために依存が逆になっている状態を許容する</remarks>
+    /// <remarks>これでも umm 以外のアセットによって AssetImporter.userData が上書きされる可能性は残るが、それは管理範疇外とみなす</remarks>
+    [XmlRoot("userData")]
+    public struct UserData
+    {
+        [XmlElement("version")] public int Version { get; set; }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Asset UserData
+
+## What
+
+* Data structure for `AssetImporter.userData`
+
+## Requirement
+
+* 
+
+## Install
+
+```shell
+yarn add "umm/asset_userdata#^1.0.0"
+```
+
+## Usage
+
+* 
+
+## License
+
+Copyright (c) 2018 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
